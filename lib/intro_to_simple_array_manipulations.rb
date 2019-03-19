0a370ed5d8722d3ccafe3e07a49a1ebaf8458e66
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-19 19:11:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,11 +1,67 @@
-using_push
-using_unshift
-using_pop
-pop_with_args
-using_shift
-using_concat
-using_insert
-using_uniq
-using_flatten
-using_delete
-using_delete_at
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift()
+  
+  
+  
+  
+end
+
+def using_pop()
+  
+  
+  
+  
+end
+
+def pop_with_args()
+  
+  
+  
+  
+end
+
+def using_shift()
+  
+  
+  
+  
+end
+
+def using_concat()
+  
+  
+  
+  
+end
+
+def using_insert()
+  
+  
+  
+  
+end
+
+def using_uniq()
+  
+  
+  
+  
+end
+
+def using_flatten()
+  
+  
+  
+  
+end
+
+def using_delete()
+  
+  
+  
+  
+end
+

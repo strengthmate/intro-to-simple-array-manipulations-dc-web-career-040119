eaebcfd70f57c23c0ae4e843ddaaacd30f136888
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-19 19:18:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,57 +2,53 @@ def using_push(array, string)
   array.push(string)
 end
 
+
 def using_unshift(array, string)
   array.unshift(string)
 end
+
 
 def using_pop(array)
   array.pop()
 end
 
+
 def pop_with_args()
   array.pop(2)
 end
+
 
 def using_shift(array)
   array.shift()
 end
 
+
 def shift_with_args (array)
   array.shift(2)
+end
 
-def using_concat(flatiron, fchool)
-  
-  
-  
+
+def using_concat(flatiron, school)
   
 end
+
 
 def using_insert()
   
-  
-  
-  
 end
+
 
 def using_uniq()
   
-  
-  
-  
 end
+
 
 def using_flatten()
   
-  
-  
-  
 end
 
+
 def using_delete()
-  
-  
-  
   
 end
 

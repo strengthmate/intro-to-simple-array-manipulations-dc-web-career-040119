b43eef8a57ff6e822f7025a48b3335d6a4936c11
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-19 19:45:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -48,8 +48,11 @@ def using_flatten(array)
 end
 
 
-def using_delete(array, int)
-  
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, int)
   array.delete_at(int)
 end
 

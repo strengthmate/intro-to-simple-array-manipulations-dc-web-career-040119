0f17a3874ff117b920f9f6a701c29304cfd5b49e
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-19 19:14:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,15 +2,15 @@ def using_push(array, string)
   array.push(string)
 end
 
-def using_unshift()
+def using_unshift(array, string)
   array.unshift(string)
 end
 
-def using_pop()
-  
+def using_pop(array)
+  array.pop
 end
 
-def pop_with_args()
+def pop_with_args(array)
   
   
   
